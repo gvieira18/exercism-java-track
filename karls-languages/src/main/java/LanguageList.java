af18e43b2e3c9c7208e51fc3a458a3e88366a3a9
Gabriel do Carmo Vieira <gabrielcarmovieira16@gmail.com>
2024-06-02 11:22:25 -0300

    feat(karls-languages): finish task

--- karls-languages/src/main/java/LanguageList.java
+++ karls-languages/src/main/java/LanguageList.java
@@ -5,30 +5,30 @@ public class LanguageList {
     private final List<String> languages = new ArrayList<>();
 
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Please implement the isEmpty() method");
+        return this.languages.isEmpty();
     }
 
     public void addLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the addLanguage() method");
+        this.languages.add(language);
     }
 
     public void removeLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
+        this.languages.remove(language);
     }
 
     public String firstLanguage() {
-        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+        return this.languages.getFirst();
     }
 
     public int count() {
-        throw new UnsupportedOperationException("Please implement the count() method");
+        return this.languages.size();
     }
 
     public boolean containsLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the containsLanguage() method");
+        return this.languages.contains(language);
     }
 
     public boolean isExciting() {
-        throw new UnsupportedOperationException("Please implement the isExciting() method");
+        return this.containsLanguage("Java") || this.containsLanguage("Kotlin");
     }
 }
