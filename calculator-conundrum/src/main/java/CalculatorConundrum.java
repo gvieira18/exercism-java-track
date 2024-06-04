3f6fa2709d38a2fe9ccad73c5930a1df2fd47d94
Gabriel do Carmo Vieira <gabrielcarmovieira16@gmail.com>
2024-06-03 22:23:32 -0300

    feat(calculator-conundrum): finish task

--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,23 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        if (operation == null) throw new IllegalArgumentException("Operation cannot be null");
+        if (operation.isEmpty()) throw new IllegalArgumentException("Operation cannot be empty");
+
+        int result;
+
+        switch (operation) {
+            case "+" -> result = operand1 + operand2;
+            case "*" -> result = operand1 * operand2;
+            case "/" -> {
+                try {
+                    result = operand1 / operand2;
+                } catch (ArithmeticException arithmeticException) {
+                    throw new IllegalOperationException("Division by zero is not allowed", arithmeticException);
+                }
+            }
+            default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+        }
+
+        return operand1 + " " + operation + " " + operand2 + " = " + result;
     }
 }
