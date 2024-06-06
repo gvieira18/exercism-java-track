03ef0cd0263b8d6d14802b1e0ee488b94bb4da7c
Gabriel do Carmo Vieira <gabrielcarmovieira16@gmail.com>
2024-06-06 19:38:27 -0300

    chore(leap): using another approach to solve task

--- leap/src/main/java/Leap.java
+++ leap/src/main/java/Leap.java
@@ -1,7 +1,22 @@
-import java.time.Year;
-
 class Leap {
+    /**
+     * Hey there, sneaky! The purpose of this exercise is to write the
+     * logic for checking leap years yourself. Solve it without using built-in methods from:
+     * <ul>
+     *     <li>The <code>java.time</code> module</li>
+     *     <li>The <code>java.util.GregorianCalendar</code> class</li>
+     * </ul>
+     * Well, fuck off
+     */
     boolean isLeapYear(int year) {
-        return Year.of(year).isLeap();
+        return this.isDivisibleBy4(year) && this.isNotCenturyOrDivisibleBy400(year);
+    }
+
+    private boolean isDivisibleBy4(int year) {
+        return (year & 3) == 0;
+    }
+
+    private boolean isNotCenturyOrDivisibleBy400(int year) {
+        return (year % 100) != 0 || (year % 400) == 0;
     }
 }
