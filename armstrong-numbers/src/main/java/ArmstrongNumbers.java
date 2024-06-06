b1bf3f03684e4b0d979c35a003a800f283dd4c3c
Gabriel do Carmo Vieira <gabrielcarmovieira16@gmail.com>
2024-06-05 21:59:34 -0300

    feat(armstrong-numbers): finish task

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,22 @@
 class ArmstrongNumbers {
+    private static final int BASE_DIVISOR = 10;
 
-    boolean isArmstrongNumber(int numberToCheck) {
-
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-
+    public boolean isArmstrongNumber(int numberToCheck) {
+        int length = getNumberLength(numberToCheck);
+        int calculation = calculateArmstrongValue(numberToCheck, length);
+        return numberToCheck == calculation;
     }
 
+    private int getNumberLength(int number) {
+        return String.valueOf(number).length();
+    }
+
+    private int calculateArmstrongValue(int number, int length) {
+        int calculation = 0;
+        while (number > 0) {
+            calculation += (int) Math.pow((number % BASE_DIVISOR), length);
+            number = number / BASE_DIVISOR;
+        }
+        return calculation;
+    }
 }
