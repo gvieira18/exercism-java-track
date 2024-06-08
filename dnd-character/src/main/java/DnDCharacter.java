d0784f6b7ba237d5649a8a67130ac178ee6ee513
Gabriel do Carmo Vieira <gabrielcarmovieira16@gmail.com>
2024-06-08 17:53:31 -0300

    feat(dnd-character): finish task

--- dnd-character/src/main/java/DnDCharacter.java
+++ dnd-character/src/main/java/DnDCharacter.java
@@ -1,44 +1,87 @@
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 class DnDCharacter {
+    private static final int INITIAL_HITPOINTS = 10;
+    private static final int TOP_SCORES_COUNT = 3;
+    private static final int NUMBER_OF_DICE_ROLLS = 4;
+    private static final int DICE_MIN_VALUE = 1;
+    private static final int DICE_MAX_VALUE = 7;
+    private static final int INITIAL_SUM = 0;
+    private static final int MODIFIER_BASE = 10;
+    private static final int DIVISOR = 2;
+
+    private final int strength;
+    private final int dexterity;
+    private final int constitution;
+    private final int intelligence;
+    private final int wisdom;
+    private final int charisma;
+    private final int hitpoints;
+
+    private final Random random;
+
+    public DnDCharacter() {
+        this.random = new Random();
+
+        this.strength = this.generateAbilityScore();
+        this.dexterity = this.generateAbilityScore();
+        this.constitution = this.generateAbilityScore();
+        this.intelligence = this.generateAbilityScore();
+        this.wisdom = this.generateAbilityScore();
+        this.charisma = this.generateAbilityScore();
+
+        this.hitpoints = INITIAL_HITPOINTS + this.modifier(this.constitution);
+    }
+
+    private int generateAbilityScore() {
+        return this.ability(this.rollDice());
+    }
 
     int ability(List<Integer> scores) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return scores.stream()
+                .sorted(Collections.reverseOrder())
+                .limit(TOP_SCORES_COUNT)
+                .reduce(INITIAL_SUM, Integer::sum);
     }
 
     List<Integer> rollDice() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.random.ints(NUMBER_OF_DICE_ROLLS, DICE_MIN_VALUE, DICE_MAX_VALUE)
+                .boxed()
+                .collect(Collectors.toList());
     }
 
-    int modifier(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public int modifier(int input) {
+        return Math.floorDivExact(Math.subtractExact(input, MODIFIER_BASE), DIVISOR);
     }
 
     int getStrength() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.strength;
     }
 
     int getDexterity() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.dexterity;
     }
 
     int getConstitution() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.constitution;
     }
 
     int getIntelligence() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.intelligence;
     }
 
     int getWisdom() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.wisdom;
     }
 
     int getCharisma() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.charisma;
     }
 
     int getHitpoints() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.hitpoints;
     }
 }
